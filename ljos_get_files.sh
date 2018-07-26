87a88bd50869538105208bb62979279e565df4ad
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-25 20:44:26 -0500

    Create ljos_get_files.sh

---
+++ ljos_get_files.sh
@@ -0,0 +1,15 @@
+# ./ljos_get_files.sh
+
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.xz
+wget https://busybox.net/downloads/busybox-1.28.3.tar.bz2
+
+wget http://ftp.osuosl.org/pub/clfs/conglomeration/clfs-embedded-bootscripts/clfs-embedded-bootscripts-1.0-pre5.tar.bz2
+wget http://ftp.gnu.org/gnu/gcc/gcc-7.3.0/gcc-7.3.0.tar.xz
+wget http://ftp.gnu.org/gnu/glibc/glibc-2.27.tar.xz
+wget http://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz
+
+wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.16.3.tar.xz
+
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
+wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.1.tar.xz
+wget http://zlib.net/zlib-1.2.11.tar.xz
