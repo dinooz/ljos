5e8c1a6c526fb0bbe17b6cceed0614d2ab297978
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-25 20:41:18 -0500

    Create ljos_get_deps.sh

---
+++ ljos_get_deps.sh
@@ -0,0 +1,20 @@
+
+# Ubuntu - Required Dependencies to compile LJOS [ Linux Journal OS ] 
+# sudo ./ljos_get_desp.sh 
+
+
+apt -y install gcc
+apt -y install make
+apt -y install libncurses5-dev
+apt -y install libncursesw5-dev
+apt -y install grub-efi-amd64
+
+apt -y install g++
+apt -y install bison
+apt -y install flex
+apt -y install libgmp3-dev
+apt -y install libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg
+apt -y install mpc
+apt -y install texinfo
+apt -y install libcloog-isl-dev
+
