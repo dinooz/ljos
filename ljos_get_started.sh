2202423253b8e035fdfe6fe3e155bf6a49018cca
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-25 20:50:47 -0500

    Create ljos_get_started.sh

---
+++ ljos_get_started.sh
@@ -0,0 +1,9 @@
+# wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_started.sh
+# chmod 755 ./ljos_get_started.sh
+# ./ljos_get_started.sh
+
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_HowTo.txt
+
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_deps.sh
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_files.sh
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_script_setup.sh
