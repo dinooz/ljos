fb1679896be87a0bc492809d8634467f6947849d
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-29 14:19:58 -0500

    Update ljos_get_started.sh

--- ljos_get_started.sh
+++ ljos_get_started.sh
@@ -4,7 +4,10 @@
 
 wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_HowTo.txt
 
-wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_deps.sh
 wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_files.sh
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_get_deps.sh
+wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_partition_swap_preps.sh
+
 wget https://raw.githubusercontent.com/dinooz/ljos/master/ljos_script_setup.sh
+
 chmod 755 ./ljos*.sh
